88a29a648dbe5fd3871083e293bdb2d4eb949603
Fernandez Emilie <emilie.fernandez1@hotmail.com>
2019-06-26 22:31:58 +0200

    ajout agenda

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { ScheduleModule } from '@syncfusion/ej2-angular-schedule';
 import { AppComponent } from './app.component';
 
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { FormsModule } from '@angular/forms';
+import { AgendaComponent } from './agenda/agenda.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AgendaComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MDBBootstrapModule.forRoot(),
     FormsModule,
+    ScheduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
