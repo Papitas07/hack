d62bdcb3980d55fdc681ee9b34b5950124b7508e
anthoiervasi <antiervasi74@gmail.com>
2019-06-27 01:59:55 +0200

    ajout composant financement et 4 conposannt projet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,23 @@ import { FormsModule } from '@angular/forms';
 import { BoiteAIdeeComponent } from './boite-aidee/boite-aidee.component';
 import { AppRoutingModule } from './app-routing.module';
 
+import { FinancementComponent } from './financement/financement.component';
+import { Financement2Component } from './financement2/financement2.component';
+import { Projet1Component } from './projet1/projet1.component';
+import { Projet2Component } from './projet2/projet2.component';
+import { Projet3Component } from './projet3/projet3.component';
+import { Projet4Component } from './projet4/projet4.component'
+
 @NgModule({
   declarations: [
     AppComponent,
-    BoiteAIdeeComponent
+    BoiteAIdeeComponent,
+    FinancementComponent,
+    Financement2Component,
+    Projet1Component,
+    Projet2Component,
+    Projet3Component,
+    Projet4Component
   ],
   imports: [
     BrowserModule,
